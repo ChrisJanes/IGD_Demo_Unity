e47d39e37133812092215fcd4d86a19d38d6df40
Chris Janes <gingoreilly@gmail.com>
2022-01-17 10:53:10 +0000

    add comments to script files

--- Assets/Scripts/PickupHandler.cs
+++ Assets/Scripts/PickupHandler.cs
@@ -7,10 +7,12 @@ public class PickupHandler : MonoBehaviour
     // handle player v pickup interactions
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // only work with objects tagged with "Pickup"
         bool isPickup = collision.gameObject.CompareTag("Pickup");
 
         if (isPickup == true)
         {
+            // disable the Pickup item rather than destroying it.
             collision.gameObject.SetActive(false);  
         } 
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,18 @@ public class PlayerController : MonoBehaviour
     // Get player input and move the attached game object
     void Update()
     {
+        // local variables that only exist inside Update
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        // create a new Vector3 that stores the updated position
         Vector3 newPosition 
             = new Vector3(
                 transform.position.x + (horizontal * MovementSpeed * Time.deltaTime),
                 transform.position.y + (vertical * MovementSpeed * Time.deltaTime), 
                 0.0f);
 
+        // override the old position with the new one
         transform.position = newPosition;
     }
 }
